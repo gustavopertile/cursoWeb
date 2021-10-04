c31659ebc57accc7433fb1172f197b55146cbbd2
Gustavo Pertile <gu.follador@gmail.com>
2021-10-04 15:49:20 -0300

    Adicionando pasta para o repositório github.

---
+++ ajax/server.js
@@ -0,0 +1,32 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.send('Concluído com sucesso.')
+    })
+})
+
+app.listen(8080, () => console.log('Executando...'))
