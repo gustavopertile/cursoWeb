dd77fcf7ef6f0c75935039cc76c06088cab743a4
Gustavo Pertile <gu.follador@gmail.com>
2021-10-04 19:23:33 -0300

    Adicionando pasta para o repositório github.

--- ajax/server.js
+++ ajax/server.js
@@ -29,4 +29,11 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
