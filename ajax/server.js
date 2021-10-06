b45025ac2d825e40308cf20b3b1ead867d10a8ab
Gustavo Pertile <gu.follador@gmail.com>
2021-10-06 11:56:16 -0300

    Adicionando pasta para o repositório github.

--- ajax/server.js
+++ ajax/server.js
@@ -36,4 +36,14 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
